bb3e7d96dde32af1930a0e3c627f3bed2789f2d6
Kajo <gattusokajo@gmail.com>
2026-04-29 18:49:57 +0800

    Update birthday message text in main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             $(this).addClass("puff-bubble");
         });
         $("#glow").remove();
-        txt.hide().html("Happy Birthday,the beautiful girl in the world. Make a wish before your happy day starts running out of time. There's not much I want to say here, but I hope everything you want will come true, and God will make things easier for you. I love you, My Star 🌟").delay(750).fadeIn(300);
+        txt.hide().html("Happy Birthday,the beautiful girl in the world. Make a wish before your happy day starts running out of time. You deserve to be celebrated, and you deserve to be loved completely. I hope some things bring you so many blessing and happiness, I'm so lucky to have you as my girlfriend. I wishing you the best, there's not much I want to say here, but I hope everything you want will come true, and God will make things easier for you. I love you, My Star 🌟").delay(750).fadeIn(300);
         $("#candle").animate(
             {
             opacity: ".5"
